da3785943cf8cd076f76bcae3a74e5d1307e7c05
Gergo Hajdu <gohajdu@gmail.com>
2018-09-21 12:40:12 -0400

    Playfair pre-check

--- src/com/gergoh/Main.java
+++ src/com/gergoh/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
         // Procedure to take input into two letter chunks
         // If uneven letters, 'Z' added to last chunk
-        ArrayList<char[]> formattedInput = new ArrayList<char[]>();
+        ArrayList<char[]> formattedInput = new ArrayList<>();
         char[] temp = new char[2];
         for (int i = 0; i < text.length(); i++) {
             for (int j = 0; j < 2; j++) {
@@ -124,10 +124,79 @@ public class Main {
     // If both letters are in the same row, take the letter to the right of each one
     // If neither of the preceding two rules are true, form a rectangle of two letters and take the letters on the horizontal opposite corner of the rectangle
     private static String encryptionProcess(char[][] keyTable, ArrayList<char[]> input) {
-        for (int i = 0; i < input.size(); i++) {
-            for (int j = 0; j < 2; j++) {
+        // Boolean value for which rule we need to use
+        boolean column = false,
+                row = false,
+                neither = false;
 
+        // Finding correct rule and finding index in key table
+        int[] firstIndex = new int[2];
+        int[] secondIndex = new int[2];
+        char [] encrypted = new char[input.size() * 2];
+        for(int i = 0; i < input.size(); i++){
+            char [] letterChunk = input.get(i);
+            findRule(keyTable, letterChunk, column, row, neither);
+            for (int r = 0; r < 5; r++) {
+                for (int c = 0; c < 5; c++) {
+                    if (letterChunk[0] == keyTable[r][c]) {
+                        firstIndex[0] = r;
+                        firstIndex[1] = c;
+                    }
+                    if (letterChunk[1] == keyTable[r][c]) {
+                        secondIndex[0] = r;
+                        secondIndex[1] = c;
+                    }
+                }
+            }
+
+            // Getting correct encrypted letter from key table
+            if(column){
+                if(firstIndex[0] == 4) firstIndex[0] = -1;
+                encrypted[i] = keyTable[firstIndex[0] + 1][firstIndex[1]];
+                i++;
+                if(secondIndex[0] == 4) secondIndex[0] = -1;
+                encrypted[i] = keyTable[secondIndex[0] + 1][secondIndex[1]];
+            }
+            if(row){
+                if(firstIndex[1] == 4) firstIndex[1] = -1;
+                encrypted[i] = keyTable[firstIndex[0]][firstIndex[1] + 1];
+                i++;
+                if(secondIndex[1] == 4) secondIndex[1] = -1;
+                encrypted[i] = keyTable[secondIndex[0]][secondIndex[1] + 1];
+            }
+            if(neither){
+                encrypted[i] = keyTable[firstIndex[0]][secondIndex[1]];
+                i++;
+                encrypted[i] = keyTable[secondIndex[0]][firstIndex[1]];
             }
         }
+
+        return new String(encrypted);
+    }
+
+    // Method iterates through input to check which rule we need to use on each chunk
+    private static void findRule(char [][] keyTable, char [] chunk, boolean column, boolean row, boolean neither) {
+        // Resetting all boolean values to false
+        column = row = neither = false;
+
+        // Storing indexes of letters of chunks in key table to later compare positions and decide rule
+        int[] firstIndex = new int[2];
+        int[] secondIndex = new int[2];
+        for (int r = 0; r < 5; r++) {
+            for (int c = 0; c < 5; c++) {
+                if (chunk[0] == keyTable[r][c]) {
+                    firstIndex[0] = r;
+                    firstIndex[1] = c;
+                }
+                if (chunk[1] == keyTable[r][c]) {
+                    secondIndex[0] = r;
+                    secondIndex[1] = c;
+                }
+            }
+        }
+
+        if (firstIndex[1] == secondIndex[1]) column = true;
+        else if(firstIndex[0] == secondIndex[0]) row = true;
+        else neither = true;
     }
 }
