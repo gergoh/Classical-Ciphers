274ae02e71a5f14276b92f3290aa0a5704c9e059
Gergo Hajdu <gohajdu@gmail.com>
2018-09-27 23:12:08 -0400

    Update

--- src/com/gergoh/Main.java
+++ src/com/gergoh/Main.java
@@ -1,6 +1,3 @@
-// TODO Make key table accessible for the future as it is needed to decrypt
-// TODO simplify findRule booleans
-// TODO Turn it into a package com.gergoh.encrypters.classical.playfairCipher
 package com.gergoh;
 
 import  com.gergoh.encrypters.classical.playfairCipher;
