40ca9f4e572787e5674227e123712a555115bdee
Gergo Hajdu <gohajdu@gmail.com>
2018-10-04 00:15:14 -0400

    Finished Rail Fence Cipher

--- src/com/gergoh/Main.java
+++ src/com/gergoh/Main.java
@@ -1,16 +1,7 @@
 package com.gergoh;
 
-import com.gergoh.encrypters.classical.RailFenceCipher;
-
-// Rail Fence Cipher testing
 public class Main {
     public static void main(String[] args) {
-        RailFenceCipher railCipher = new RailFenceCipher();
-
-        railCipher.encrypt();
-
-        railCipher.outputCipherText();
-
 
     }
 }
